83fdca92de69a6161b39218d63a66a774feb1e3e
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-02-05 20:21:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,6 +6,7 @@ def line(katz_deli)
   else
     current_line = ("The line is currently: ")
     katz_deli.each_with_index do |person, index|
+    current_line << " #{index + 1}. #{name}"
   end
   puts current_line
 end
