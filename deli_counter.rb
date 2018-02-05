852bc34857f4f088d42951e013233580266954ef
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-02-05 19:48:04 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
-def line
-  katz_deli = []
-  if katz_deli == 0
-  puts ("The line is currently empty.")
+def line(katz_deli)
+    if katz_deli.size == 0
+    puts ("The line is currently empty.")
+  
 end
 end
