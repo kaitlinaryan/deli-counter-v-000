5bed863e703f06ebe51185b239dff50339ade524
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-02-05 20:05:51 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,17 @@
+katz_deli = []
+
 def line(katz_deli)
-    if katz_deli.size == 0
+    if katz_deli.length == 0
     puts ("The line is currently empty.")
-  
+  else katz_deli.each_with_index 
+    puts ("The line is currently: #{index}")
 end
+end
+
+
+
+def take_a_number(current_line, name)
+end
+
+def now_serving
 end
