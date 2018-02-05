b76f0916dfa5d8294bb3d859ee30cb2d8df7dfaf
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-02-05 20:49:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,3 +11,11 @@ def line(deli_line)
   puts current_line
 end
 end
+
+def take_a_number(deli_line, person)
+if deli_line.length == 0
+  puts ("Hello #{name}, you are #{index +1} in line")
+  else deli_line.each_with_index do |person, index|
+  deli_line << " #{person} #{index + 1}. "
+end
+end
